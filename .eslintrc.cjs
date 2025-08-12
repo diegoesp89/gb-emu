ffc0e2ae864ec9ac17067c41aee7aa0ae20ca31a
diego espinoza <despinozav@falabella.cl>
2025-08-11 23:14:28 -0400

    Feat: First Commit Motherfucka
    .
    
    :q
    '

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',           
+  ],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    // Opcional: ajustar según preferencias
+    '@typescript-eslint/no-unused-vars': ['warn'],
+  },
+};
